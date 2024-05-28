eec6321ddeda444a8dd5e6e301f8e29015e92dac
panjiprann <panjipranantya20@gmail.com>
2024-05-28 15:01:35 +0700

    menyelesaikan class budi

--- paradigma2.cpp
+++ paradigma2.cpp
@@ -34,5 +34,11 @@ class pelajar : public orang {
 
 class budi : public pekerja, public pelajar {
     public:
-    
-}
+
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur)
+    {
+        cout << "budi dibuat\n" << endl;
+    }
+};
