57ea5f331a1445234ff7696d606882344e45b35a
panjiprann <panjipranantya20@gmail.com>
2024-05-28 15:54:01 +0700

    menyelesaikan int main

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class remoteLampu {
     private:
-    string saklarNo{10};
+    string saklarNo[10];
     public:
     void setSaklarNo(int i, string value) {
         saklarNo[i] = value;
@@ -17,6 +17,13 @@ int main() {
     remoteLampu lampuRumah;
 
     lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
-    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kasar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
 
+    cout << lampuRumah.getSaklarNo(0) << endl;
+    cout << lampuRumah.getSaklarNo(1) << endl;
+    cout << lampuRumah.getSaklarNo(2) << endl;
+    cout << lampuRumah.getSaklarNo(3) << endl;
+    return 0;
 }
